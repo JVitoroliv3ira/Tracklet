2c8206e323b861e4c3dff800104254776e0b3108
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-03-27 20:58:59 -0300

    feat: Configura conexão com banco de dados

--- Tracklet.Api/Program.cs
+++ Tracklet.Api/Program.cs
@@ -1,8 +1,23 @@
+using FluentMigrator.Runner;
+using Microsoft.EntityFrameworkCore;
+using Tracklet.Infra.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var applicationConnectionString = builder.Configuration.GetConnectionString("ApplicationConnection") ??
+                                  throw new Exception("Application connection string not found");
+var migrationConnectionString = builder.Configuration.GetConnectionString("MigrationConnection") ??
+                                throw new NullReferenceException("Migration connection string not found");
+
+builder.Services.AddDbContext<ApplicationDbContext>(o =>
+    o.UseNpgsql(applicationConnectionString, op => op.EnableRetryOnFailure())
+);
+
+builder.Services.AddFluentMigrator(migrationConnectionString);
 
 var app = builder.Build();
 
@@ -10,6 +25,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+    runner.MigrateUp();
 }
 
 app.UseAuthorization();
