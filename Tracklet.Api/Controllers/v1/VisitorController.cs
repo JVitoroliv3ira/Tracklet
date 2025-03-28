cce0e34d5b0dd4e3467373ab0e51467fbc3b9321
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-03-27 22:52:40 -0300

    feat: Adiciona VisitorController

---
+++ Tracklet.Api/Controllers/v1/VisitorController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tracklet.Api.Controllers.v1;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class VisitorController : ControllerBase
+{
+    
+}
